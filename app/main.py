cff404cbcc5bbf133a76ed7d77beb2dc0f13aae1
Jefri Adventer <jefri.adventer@bukalapak.com>
2021-10-27 00:36:55 +0700

    add third version of app

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from datetime import date
+import psutil
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    start = '25/10/2021'
-    today = date.today()
-    return f'Xendit - Trial - Jefri Adventer - {start} - {today.strftime("%d/%m/%Y")}'
+    cpu = psutil.cpu_percent()
+    memory = psutil.virtual_memory()[2]
+    return f'CPU = {cpu}<br/>Memory = {memory}'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
