bb703f3d7ad134edb7ffbc3a66665d18319665fe
Jefri Adventer <jefri.adventer@bukalapak.com>
2021-10-27 13:52:44 +0700

    test re-run the workflow from app v1

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 from flask import Flask
-import psutil
+import distro
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 @app.route("/")
 def index():
-    cpu = psutil.cpu_percent()
-    memory = psutil.virtual_memory()[2]
-    return f'CPU = {cpu}<br/>Memory = {memory}'
+    return distro.info()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
